f4f79f54903b862ce3012bb6bbdc2b32e5f5328e
nick <nick@nick-big-laptop.(none)>
2013-10-22 19:43:54 -0500

    Minor fix ... was excluding the wrong file

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 require('fs').readdirSync('./').forEach(function(filename){
   if(filename === 'init.js') return;
-  if(filename === 'array_manip.js') return;
+  if(filename === 'add_helper.js') return;
   if(filename.slice(-3) !== '.js') return;
   console.log();
   console.log(filename);
